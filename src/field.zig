d7afeeaf504c756fdc77a11ba0584657cb278835
Ed Yu <edyu@users.noreply.github.com>
2023-07-21 13:25:24 -0700

    Add binary finite field

---
+++ src/field.zig
@@ -0,0 +1,106 @@
+const std = @import("std");
+
+pub const Self = @This();
+
+exp: u8,
+order: u8,
+divisor: u8,
+
+pub const ValueError = error{
+    InvalidExponentError,
+    InvalidNumberError,
+    NoInverseError,
+};
+
+// 2^n field
+pub fn init(n: usize) ValueError!Self {
+    var d: u8 = undefined;
+
+    // Irreducible polynomail for mod multiplication
+    d = switch (n) {
+        1 => 3, // 1 + x ? undef  shift(0b11)=2
+        2 => 7, // 1 + x + x^2    shift(0b111)=3
+        3 => 11, // 1 + x + x^3   shift(0b1011)=4
+        4 => 19, // 1 + x + x^4   shift(0b10011)=5
+        5 => 37, // 1 + x^2 + x^5 shift(0b100101)=6
+        6 => 67, // 1 + x + x^6   shift(0b1000011)=7
+        7 => 131, // 1 + x + x^7  shift(0b10000011)=8
+        else => return ValueError.InvalidExponentError,
+    };
+
+    return .{
+        .exp = @intCast(n),
+        .divisor = d,
+        .order = @as(u8, 1) << @intCast(n),
+    };
+}
+
+pub fn validated(self: *const Self, a: usize) ValueError!u8 {
+    if (a < self.order) {
+        return @intCast(a);
+    } else {
+        return ValueError.InvalidNumberError;
+    }
+}
+
+pub fn add(self: *const Self, a: usize, b: usize) ValueError!u8 {
+    return try self.validated((try self.validated(a)) ^ (try self.validated(b)));
+}
+
+pub fn neg(self: *const Self, a: usize) ValueError!u8 {
+    return try self.validated(a);
+}
+
+pub fn sub(self: *const Self, a: usize, b: usize) ValueError!u8 {
+    return try self.add(a, try self.neg(b));
+}
+
+fn countBits(num: usize) u8 {
+    var n = num;
+    var c: u8 = 0;
+    while (n != 0) {
+        n >>= 1;
+        c += 1;
+    }
+    return c;
+}
+
+pub fn mul(self: *const Self, a: usize, b: usize) ValueError!u8 {
+    if (self.exp == 1) {
+        return self.validated(try self.validated(a) * try self.validated(b));
+    }
+
+    // n > 1
+    const x = try self.validated(a);
+    const y = try self.validated(b);
+    var result: u16 = 0;
+    for (0..8) |i| {
+        const n = 7 - i;
+        if (((y >> @intCast(n)) & 1) == 1) {
+            result ^= @as(u16, x) << @intCast(n);
+        }
+    }
+    while (result >= self.order) {
+        // count how many binary digits result has
+        var n = countBits(result);
+        n -= self.exp + 1;
+        result ^= @as(u16, self.divisor) << @intCast(n);
+    }
+    return try self.validated(result);
+}
+
+pub fn invert(self: *const Self, a: usize) ValueError!u8 {
+    if (try self.validated(a) == 0) {
+        return ValueError.NoInverseError;
+    }
+    for (0..self.order) |b| {
+        if (try self.mul(a, b) == 1) {
+            return try self.validated(b);
+        }
+    }
+    return ValueError.NoInverseError;
+}
+
+pub fn div(self: *const Self, a: usize, b: usize) ValueError!u8 {
+    return try self.mul(a, try self.invert(b));
+}
--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,95 @@
 const std = @import("std");
 const testing = std.testing;
+const Field = @import("field.zig");
 
-export fn add(a: i32, b: i32) i32 {
-    return a + b;
+fn initTest(allocator: std.mem.Allocator) !std.ArrayList(Field) {
+    var fields = try std.ArrayList(Field).initCapacity(allocator, 7);
+
+    for (1..8) |n| {
+        try fields.append(try Field.init(n));
+    }
+
+    return fields;
 }
 
 test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+    const fields = try initTest(testing.allocator);
+    defer fields.deinit();
+    for (fields.items) |field| {
+        var negatives = std.AutoHashMap(usize, usize).init(testing.allocator);
+        defer negatives.deinit();
+        for (0..field.order) |a| {
+            for (0..field.order) |b| {
+                var result = try field.add(a, b);
+                try testing.expect(result < field.order);
+                try testing.expectEqual(result, try field.add(b, a));
+                if (result == 0) {
+                    try negatives.put(a, b);
+                    try negatives.put(b, a);
+                }
+            }
+        }
+        try testing.expectEqual(@as(u32, field.order), negatives.count());
+    }
+}
+
+test "basic subtract functionality" {
+    const fields = try initTest(testing.allocator);
+    defer fields.deinit();
+    for (fields.items) |field| {
+        for (0..field.order) |a| {
+            for (0..field.order) |b| {
+                var result = try field.sub(a, b);
+                try testing.expect(result < field.order);
+                if (a == b) {
+                    try testing.expectEqual(result, 0);
+                } else {
+                    try testing.expect(result > 0);
+                }
+            }
+        }
+    }
+}
+
+test "basic multiply functionality" {
+    const fields = try initTest(testing.allocator);
+    defer fields.deinit();
+    for (fields.items) |field| {
+        var reciprocals = std.AutoHashMap(usize, usize).init(testing.allocator);
+        defer reciprocals.deinit();
+        for (0..field.order) |a| {
+            for (0..field.order) |b| {
+                var result = try field.mul(a, b);
+                try testing.expectEqual(result, try field.mul(b, a));
+                if (result == 0) {
+                    try testing.expect(a == 0 or b == 0);
+                }
+                if (result == 1) {
+                    try testing.expect(a != 0 and b != 0);
+                    try reciprocals.put(a, b);
+                    try reciprocals.put(b, a);
+                }
+            }
+        }
+        try testing.expectEqual(@as(u32, field.order - 1), reciprocals.count());
+    }
+}
+
+test "basic divide functionality" {
+    const fields = try initTest(testing.allocator);
+    defer fields.deinit();
+    for (fields.items) |field| {
+        for (0..field.order) |a| {
+            for (0..field.order) |b| {
+                if (b == 0) continue;
+                var result = try field.div(a, b);
+                try testing.expect(result < field.order);
+                if (a == b) {
+                    try testing.expectEqual(result, 1);
+                } else {
+                    try testing.expect(result != 1);
+                }
+            }
+        }
+    }
 }
