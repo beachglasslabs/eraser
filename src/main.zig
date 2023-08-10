228061bfff25301718b0522838390a822c87742e
Ed Yu <edyu@users.noreply.github.com>
2023-08-10 20:52:21 +1000

    Export everything through main

--- src/main.zig
+++ src/main.zig
@@ -1,2 +1,14 @@
 const std = @import("std");
-const testing = std.testing;
+
+pub const DataOrder = @import("matrix.zig").DataOrder;
+pub const Matrix = @import("matrix.zig").Matrix;
+pub const BinaryFiniteField = @import("finite_field.zig").BinaryFiniteField;
+pub const BinaryFieldMatrix = @import("field_matrix.zig").BinaryFieldMatrix;
+pub const ErasureCoder = @import("erasure.zig").ErasureCoder;
+
+test {
+    _ = @import("matrix.zig");
+    _ = @import("finite_field.zig");
+    _ = @import("field_matrix.zig");
+    _ = @import("erasure.zig");
+}
